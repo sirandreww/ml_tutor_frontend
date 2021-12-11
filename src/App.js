38a3dc85abb450d9880ff2dde1f43b05681739fc
Your Name <you@example.com>
2021-12-11 19:48:43 +0200

    david's changes

--- src/App.js
+++ src/App.js
@@ -1,24 +1,21 @@
-import logo from './logo.svg';
 import './App.css';
+import Navbar from './components/Navbar.js';
+import React from 'react';
+import Home from './components/pages/Home';
+import Service from './components/pages/Service';
+import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <>
+      <Router>
+        <Navbar />
+        <Routes>
+          <Route path='/' exact element={<Home/>} />
+          <Route path='/service' element={<Service/>} />
+        </Routes>
+      </Router>
+    </>
   );
 }
 
---
+++ src/components/Navbar.js
@@ -0,0 +1,98 @@
+import React, {useState, useEffect} from 'react'
+import { Link } from 'react-router-dom'
+import logo from '../MlLogo.png'
+import {Button} from './Button';
+import './Navbar.css';
+
+
+function Navbar() {
+    const [click, setClick] = useState(false);
+    const [button, setButton] = useState(true);
+  
+    const handleClick = () => setClick(!click);
+    const closeMobileMenu = () => setClick(false);
+  
+    const showButton = () => {
+      if (window.innerWidth <= 960) {
+        setButton(false);
+      } else {
+        setButton(true);
+      }
+    };
+
+    useEffect(() => {
+      showButton();
+    }, []);
+
+    window.addEventListener('resize', showButton);
+
+    return (
+        <>
+        <nav className="navbar">
+            <div className='navbar-container'>
+                <Link to='/' className='navbar-logo' onClick={closeMobileMenu}>
+                    MLomda
+                    {/*Image for the logo in navbar */} 
+                    <img src={logo} alt="Logo" width="40" height="40"/>
+                </Link> 
+                <div className='menu-icon' onClick={handleClick}>
+                    {/*icons on clicking the menu*/} 
+                    <i className={click ? 'fas fa-times' : 'fas fa-bars'} />
+                </div>
+                <ul className={click ? 'nav-menu active' : 'nav-menu'}>
+                  {/*Add components to the list by adding li header */}
+                  <li className='nav-item'>
+                    <Link to='/' 
+                          className='nav-links'
+                          onClick={closeMobileMenu}
+                    >
+                      Home
+                    </Link>
+                  </li>
+                  <li className='nav-item'>
+                    <Link
+                      to='/service'
+                      className='nav-links'
+                      onClick={closeMobileMenu}
+                    >
+                      Service
+                    </Link>
+                  </li>
+                  <li className='nav-item'>
+                    <Link
+                      to='/algorithms'
+                      className='nav-links'
+                      onClick={closeMobileMenu}
+                    >
+                      Algorithms
+                    </Link>
+                  </li>
+                  <li className='nav-item'>
+                    <Link
+                      to='/sign-in'
+                      className='nav-links'
+                      onClick={closeMobileMenu}
+                    >
+                      Sign In
+                    </Link>
+                  </li>
+
+                  <li>
+                    <Link
+                      to='/sign-up'
+                      className='nav-links-mobile'
+                      onClick={closeMobileMenu}
+                    >
+                      Sign Up
+                    </Link>
+                  </li>
+          </ul>
+          {button && <Button buttonStyle='btn--outline'>SIGN UP</Button>}
+        </div>
+      </nav>
+    </>
+  );   
+}
+
+export default Navbar;
+
