35782c6668250a3fb6e3f9a72fbc3eb239aaa102
Your Name <you@example.com>
2021-12-23 22:11:48 +0200

    fade in text on hero screen

--- src/pages/Home/utils/HeroSection.js
+++ src/pages/Home/utils/HeroSection.js
@@ -1,7 +1,7 @@
 import * as React from 'react';
 import Button from '@mui/material/Button';
 import Typography from '@mui/material/Typography';
-import { Box, Container, Stack } from '@mui/material';
+import { Box, Container, Stack, Fade } from '@mui/material';
 import backgroundImage from 'assets/images/home_img_2.png';
 import { Link } from 'react-router-dom';
 
@@ -14,22 +14,27 @@ export default function HeroSection() {
         backgroundRepeat: 'no-repeat',
         pt: 20,
         pb: 6,
-        minHeight: 400,
+        minHeight: 700,
       }}
     >
-      <Container maxWidth="sm">
-        <Typography
-          component="h1"
-          variant="h2"
-          align="center"
-          color="white"
-          gutterBottom
-        >
-          MLomda
-        </Typography>
-        <Typography variant="h5" align="center" color="#79B4B7" paragraph>
-          Understand how Machine Learning works by using 3D animation and simple explanations.
-        </Typography>
+      <Container maxWidth="sm" >
+        <Fade in={Boolean(true)} timeout={5000} >
+          <Typography
+            component="h1"
+            variant="h1"
+            align="center"
+            color="white"
+            fontFamily={"PT Sans"}
+            gutterBottom
+          >
+            MLomda
+          </Typography>
+        </Fade>
+        <Fade style={{ transitionDelay: '2000ms' }} in={Boolean(true)} timeout={5000}>
+          <Typography variant="h4" align="center" color="#79B4B7" paragraph fontFamily={"PT Sans"}>
+            Understand how Machine Learning works by using 3D animation and simple explanations.
+          </Typography>
+        </Fade>
         <Stack
           sx={{ pt: 4 }}
           direction="row"
