0cefdbba2a4bdd60abdb43f64f649b034d8440a8
AseelAborokn <AseelAborokn@users.noreply.github.com>
2022-01-11 13:49:09 +0200

    moved the functions to their proper places 1D/2D and kept only the shared functions in helper.js

--- src/pages/algorithms/gradient_descent/slides/GradientDescent1D.js
+++ src/pages/algorithms/gradient_descent/slides/GradientDescent1D.js
@@ -2,11 +2,114 @@
 import * as React from 'react';
 import Box from '@mui/material/Box';
 import Grid from "@mui/material/Grid";
+import functionPlot from "function-plot";
 import { button, LeftItem, CenterItem, RightItem } from 'pages/algorithms/dashboard/utils'
 import Typography from '@mui/material/Typography';
 import QuestionTable from 'pages/algorithms/dashboard/QuestionTable';
-import { getPoints1D, getGraph1D, PrettoSlider, getDev, HEADERS_1D, getExample, getAnswers1D } from 'pages/algorithms/gradient_descent/helper';
+import { PrettoSlider, getDev, getExample, math, DIGITS } from 'pages/algorithms/gradient_descent/helper';
 // --------------------------------------------------------
+
+export const HEADERS_1D = [
+    ['step', 'Step'],
+    ['x', 'x'],
+    ['dx', "f'(x)"],
+    ['tmpX', "alpha * f'(x)"],
+    ['newX', "x'"],
+]
+
+function getPoints1D(f, startX, steps_count, alpha) {
+    var points = [[startX, math.evaluate(f, { 'x': startX })]]
+    var df = getDev(f, 'x')
+    // console.log("f=", f, "df=", df, " startX=", startX, " steps_count=", steps_count, " alpha=", alpha)
+
+    startX = Number(startX)
+    steps_count = Number(steps_count)
+    alpha = Number(alpha)
+
+
+    var prev = startX
+    for (let i = 0; i < steps_count; i++) {
+        // console.log("i=", i)
+        var tmp = math.evaluate('alpha*('.concat(df).concat(')'), { 'alpha': alpha, 'x': prev })
+        // console.log("alpha*df = ", tmp) 
+        var next = math.evaluate('prev-tmp', { 'prev': prev, 'tmp': tmp })
+        // console.log("next = ", next)
+        points.push([next, math.evaluate(f, { 'x': next })])
+        prev = next
+    }
+
+    // console.log('points=', points)
+    return points
+}
+
+function getGraph1D(f, points) {
+    var width = 800;
+    var height = 500;
+    // console.log("points= \n", points)
+    // console.log("f= \n", f)
+
+    functionPlot({
+        target: '#graph-board',
+        width,
+        height,
+        xAxis: { domain: [-(math.abs(points[0][0]) + 2), math.abs(points[0][0]) + 2], label: 'x' },
+        yAxis: { domain: [-(math.abs(points[0][1]) + 2), math.abs(points[0][1]) + 2], label: 'f(x)' },
+        title: f,
+        grid: true,
+        data: [
+            {
+                fn: f,
+                derivative: {
+                    fn: getDev(f, 'x'),
+                    updateOnMouseMove: true
+                },
+            },
+            {
+                points: points,
+                fnType: 'points',
+                graphType: 'polyline',
+            }
+        ]
+    });
+}
+
+function getAnswers1D(header, rows, f, startX, alpha) {
+    try {
+        let keys = header.map((ele) => ele[0]);
+        let res = {}
+        keys.forEach(key => res[key] = [])
+
+        var df = getDev(f, 'x')
+        startX = Number(startX)
+        alpha = Number(alpha)
+
+        var prev = startX
+        for (let i = 0; i < rows; i++) {
+            var ans = {
+                step: i,
+                x: prev,
+                dx: Number(math.evaluate(df, { 'x': prev })).toFixed(DIGITS),
+                tmpX: null,
+                newX: null,
+            }
+            ans.tmpX = Number(math.evaluate('alpha*('.concat(df).concat(')'), { 'alpha': alpha, 'x': prev })).toFixed(DIGITS)
+            ans.newX = Number(math.evaluate('prev-tmp', { 'prev': prev, 'tmp': ans.tmpX })).toFixed(DIGITS)
+
+            for (const [key, value] of Object.entries(ans)) {
+                res[key].push(value)
+            }
+
+            prev = ans.newX
+
+        }
+        // ['', x, math.evaluate(df, {'x': x}), dfx, math.evaluate('x-tmp', {'x': x, 'tmp': dfx})]
+
+        return res
+    }
+    catch (e) {
+        console.log('error at getAnswers1D(header, rows, f, startX, alpha) => \n', e)
+    }
+}
 
 export default function GradientDescent1D(props) {
 
--- src/pages/algorithms/gradient_descent/slides/GradientDescent2D.js
+++ src/pages/algorithms/gradient_descent/slides/GradientDescent2D.js
@@ -2,11 +2,196 @@
 import * as React from 'react';
 import Box from '@mui/material/Box';
 import Grid from "@mui/material/Grid";
+import Plotly from 'plotly.js-dist-min';
 import { button, LeftItem, CenterItem } from 'pages/algorithms/dashboard/utils'
 import Typography from '@mui/material/Typography';
 import QuestionTable from 'pages/algorithms/dashboard/QuestionTable';
-import { HEADERS_2D, getData2D, getPoints2D, getGraph2D, getDev, getExample, getAnswers2D, PrettoSlider } from '../helper';
+import { getDev, getExample, PrettoSlider, math, DIGITS } from '../helper';
 // --------------------------------------------------------
+
+export const HEADERS_2D = [
+    ['step', 'Step'],
+    ['x', 'x'],
+    ['y', 'y'],
+    ['dx', "dfx(x, y)"],
+    ['dy', "dfy(x, y)"],
+    ['tmpX', "alpha * dfx(x, y)"],
+    ['tmpY', "alpha * dfy(x, y)"],
+    ['newX', "x'"],
+    ['newY', "y'"],
+]
+
+
+function getPoints2D(f, startX, startY, steps_count, alpha) {
+    try {
+        var dfx = getDev(f, 'x')
+        var dfy = getDev(f, 'y')
+        var prevX = Number(startX)
+        var prevY = Number(startY)
+        steps_count = Number(steps_count)
+        alpha = Number(alpha)
+
+        var points = {
+            x: [prevX],
+            y: [prevY],
+            z: [math.evaluate(f, { 'x': startX, 'y': startY })]
+        }
+        // console.log("f=", f, "dfx=", dfx, "dfy=", dfy, " startX=", startX, " steps_count=", steps_count, " alpha=", alpha)
+
+        for (let i = 0; i < steps_count; i++) {
+            // console.log("i=", i)
+
+            var tmpX = math.evaluate('alpha*('.concat(dfx).concat(')'), { 'alpha': alpha, 'x': prevX, 'y': prevY })
+            var tmpY = math.evaluate('alpha*('.concat(dfy).concat(')'), { 'alpha': alpha, 'x': prevX, 'y': prevY })
+            var nextX = math.evaluate('prevX-tmpX', { 'prevX': prevX, 'tmpX': tmpX })
+            var nextY = math.evaluate('prevY-tmpY', { 'prevY': prevY, 'tmpY': tmpY })
+            var z = math.evaluate(f, { 'x': nextX, 'y': nextY })
+
+            // console.log('prevX=', prevX, ' prevY=', prevY, ' tmpX=', tmpX, ' tmpY=', tmpY, ' nextX=', nextX, ' nextY=', nextY )
+            points.x.push(nextX)
+            points.y.push(nextY)
+            points.z.push(z)
+
+            prevX = nextX
+            prevY = nextY
+        }
+
+        // console.log("points = ", points)
+        return points
+
+    }
+    catch (e) {
+        console.log('error at getPoints2D(f, startX, startY, steps_count, alpha) => \n', e)
+        return '0'
+    }
+}
+
+function getGraph2D(data, points) {
+    try {
+        // console.log('getGraph2D - \n')
+        // console.log('data = ', data, '\n')
+        // console.log('points = ', points, '\n')
+
+        var z = []
+
+        for (let y = -10; y < 11; y += 1) {
+            var new_y = []
+            for (let x = -10; x < 11; x += 1) {
+                new_y.push(x)
+            };
+            z.push(new_y)
+        }
+        const data_z1 = {
+            type: 'surface',
+            x: data.x,
+            y: data.y,
+            z: data.z,
+            colorscale: 'Viridis',
+            lighting: {
+                roughness: 0.2
+            }
+        };
+        const data_z2 = {
+            type: 'scatter3d',
+            mode: 'points',
+            x: points.x,
+            y: points.y,
+            z: points.z,
+            marker: { color: 'red' }
+        };
+        const layout = {
+            xaxis: {
+                range: [-5, 5]
+            },
+            yaxis: {
+                range: [-5, 5]
+            }
+        }
+        var config = { responsive: true }
+        Plotly.newPlot('graph2-board', [data_z1, data_z2], layout, config);
+    }
+    catch (e) {
+        console.log('error at getGraph2D(data, points) => \n', e)
+        return '0'
+    }
+}
+
+function getAnswers2D(header, rows, f, startX, startY, alpha) {
+    try {
+        let keys = header.map((ele) => ele[0]);
+        let res = {}
+        keys.forEach(key => res[key] = [])
+
+        var dfx = getDev(f, 'x')
+        var dfy = getDev(f, 'y')
+        startX = Number(startX).toFixed(DIGITS)
+        startY = Number(startY).toFixed(DIGITS)
+        alpha = Number(alpha)
+
+        var prevX = startX
+        var prevY = startY
+
+        for (let i = 0; i < rows; i++) {
+            var ans = {
+                step: i,
+                x: prevX,
+                y: prevY,
+                dx: Number(math.evaluate(dfx, { 'x': prevX, 'y': prevY })).toFixed(DIGITS),
+                dy: Number(math.evaluate(dfy, { 'x': prevX, 'y': prevY })).toFixed(DIGITS),
+                tmpX: null,
+                tmpY: null,
+                newX: null,
+                newY: null,
+            }
+            ans.tmpX = Number(math.evaluate('alpha*('.concat(dfx).concat(')'), { 'alpha': alpha, 'x': prevX, 'y': prevY })).toFixed(DIGITS)
+            ans.newX = Number(math.evaluate('prevX-tmpX', { 'prevX': prevX, 'tmpX': ans.tmpX })).toFixed(DIGITS)
+            ans.tmpY = Number(math.evaluate('alpha*('.concat(dfy).concat(')'), { 'alpha': alpha, 'x': prevX, 'y': prevY })).toFixed(DIGITS)
+            ans.newY = Number(math.evaluate('prevY-tmpY', { 'prevY': prevY, 'tmpY': ans.tmpY })).toFixed(DIGITS)
+
+            for (const [key, value] of Object.entries(ans)) {
+                res[key].push(value)
+            }
+
+            prevX = ans.newX
+            prevY = ans.newY
+
+        }
+        // ['', x, math.evaluate(df, {'x': x}), dfx, math.evaluate('x-tmp', {'x': x, 'tmp': dfx})]
+        // console.log('res=', res)
+        return res
+    }
+    catch (e) {
+        console.log('error at getAnswers2D(header, rows, f, startX, startY, alpha) => \n', e)
+    }
+
+}
+
+function getData2D(f) {
+    try {
+        var data = {
+            x: [],
+            y: [],
+            z: []
+        }
+
+        for (let y = -10; y < 11; y += 1) {
+            var new_y = [[], []]
+            for (let x = -10; x < 11; x += 1) {
+                new_y[0].push(math.evaluate(f, { 'x': x, 'y': y }))
+                new_y[1].push(x)
+            };
+            data.x.push(new_y[1])
+            data.y.push(y)
+            data.z.push(new_y[0])
+        }
+
+        return data
+    }
+    catch (e) {
+        console.log('error at getData2D(f) => \n', e)
+        return '0'
+    }
+}
 
 export default function GradientDescent2D(props) {
 
