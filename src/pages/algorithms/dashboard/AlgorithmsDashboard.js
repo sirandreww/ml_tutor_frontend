d6fbf5b59e5eea90eb94749999a7ecd1669d5d04
sirandreww <andrew.luka.ia@gmail.com>
2022-01-23 23:10:34 +0200

    splitting to multiple pages

--- src/pages/algorithms/dashboard/AlgorithmsDashboard.js
+++ src/pages/algorithms/dashboard/AlgorithmsDashboard.js
@@ -2,9 +2,13 @@ import * as React from 'react';
 import Container from '@mui/material/Container';
 import Navbar from 'components/Navbar.js';
 import Footer from 'components/Footer.js';
-import { Box } from '@mui/material';
+import { Box, Button } from '@mui/material';
+import { Link } from 'react-router-dom';
+import { useTranslation } from "react-i18next";
 
 export default function AlgorithmsDashboard(props) {
+  const [t] = useTranslation(['translation']);
+
   return (
     <div>
       <Navbar />
@@ -14,12 +18,33 @@ export default function AlgorithmsDashboard(props) {
 
         {/* actual content */}
         <Box sx={{ width: '100%' }}>
-          <React.Fragment>
-            <Container maxWidth="md">
-              {props.component}
-            </Container>
-          </React.Fragment>
-        </Box>      
+          <Container maxWidth="md">
+            <Box height={50} />
+            {props.component}
+            <Box height={50} />
+          </Container>
+        </Box>
+
+        {/* slide managing code */}
+        <Box sx={{ display: 'flex', flexDirection: 'row', pt: 2 }}>
+          <Link to={props.previous} style={{ textDecoration: 'none', color: "black" }}>
+            <Button
+              color="inherit"
+              disabled={props.isPreviousDisabled}
+              sx={{ mr: 1 }}
+            >
+              {t("back")}
+            </Button>
+          </Link>
+          <Box sx={{ flex: '1 1 auto' }} />
+          <Link to={props.next} style={{ textDecoration: 'none', color: "black" }}>
+            <Button >
+              {props.isNextDisabled ? t("finish") : t("next")}
+            </Button>
+          </Link>
+        </Box>
+        <Box height={50} />
+
       </Container>
       <Footer />
     </div>
