0563a4b219cb20db755d4737a71cf55e3a8ce460
sirandreww <andrew.luka.ia@gmail.com>
2022-01-23 19:22:15 +0200

    refactoring code to get 0 warnings

--- src/App.js
+++ src/App.js
@@ -6,8 +6,11 @@ import { createTheme, ThemeProvider } from '@mui/material/styles';
 import ScrollToTop from './components/ScrollToTop.js';
 import AlgorithmsDashboard from 'pages/algorithms/dashboard/AlgorithmsDashboard.js';
 
+// translation imports
 import i18next from "i18next";
-import {useTranslation} from "react-i18next";
+import { I18nextProvider, initReactI18next } from 'react-i18next';
+import translationsEn from './translations/translationsEn.json';
+import translationsHe from './translations/translationsHe.json';
 
 const theme = createTheme({});
 
@@ -16,35 +19,48 @@ const Algorithms = lazy(() => import('pages/algorithms/Algorithms.js'));
 const SignIn = lazy(() => import('pages/sign/SignIn.js'));
 // const GD = lazy(() => import('pages/sign/SignIn.js'));
 //<AlgorithmsDashboard component={<GradientDescent />} />
-function App() {
-  const {t} = useTranslation(['translation']);
 
+i18next
+  .use(initReactI18next)
+  .init({
+    resources: {
+      en: { translation: translationsEn },
+      he: { translation: translationsHe },
+    },
+    lng: "en",
+    fallbackLng: "en",
+    interpolation: { escapeValue: false },
+  });
+
+function App() {
   const onChange = (event) => {
     i18next.changeLanguage(event.target.value);
-    
+
   }
   document.body.dir = i18next.dir();
 
   return (
-    <ThemeProvider theme={theme}>
-      <Router>
-        <Suspense fallback={<div>Loading...</div>}>
-          <ScrollToTop />
-          <Routes >
-            <Route path='/' exact element={<Home />} />
-            <Route path='/algorithms' element={<Algorithms />} />
-            <Route path='/algorithms/gd' element={<AlgorithmsDashboard component={<GradientDescent />} />} />
-            <Route path='/signin' element={<SignIn />} />
-          </Routes>
-        </ Suspense>
-      </Router>
+    <I18nextProvider i18n={i18next}>
+      <ThemeProvider theme={theme}>
+        <Router>
+          <Suspense fallback={<div>Loading...</div>}>
+            <ScrollToTop />
+            <Routes >
+              <Route path='/' exact element={<Home />} />
+              <Route path='/algorithms' element={<Algorithms />} />
+              <Route path='/algorithms/gd' element={<AlgorithmsDashboard component={<GradientDescent />} />} />
+              <Route path='/signin' element={<SignIn />} />
+            </Routes>
+          </ Suspense>
+        </Router>
 
-      <select name="language" onChange={onChange}>
+        <select name="language" onChange={onChange}>
           <option value="en">English</option>
           <option value="he">עברית</option>
-      </select>
+        </select>
 
-    </ThemeProvider>
+      </ThemeProvider>
+    </I18nextProvider>
   );
 }
 
