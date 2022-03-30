0f0448c17b6437134790f823172291cf891b8661
sirandreww <andrew.luka.ia@gmail.com>
2022-03-30 10:42:22 +0300

    fixes

--- src/components/AlgorithmsDashboard.tsx
+++ src/components/AlgorithmsDashboard.tsx
@@ -21,12 +21,6 @@ const gd_steps: string[] = [
 const lr_steps: string[] = [
   "1d_int",
   "1d_vis",
-  "1d_sbs",
-  "1d_hyp",
-  "2d_int",
-  "2d_vis",
-  "2d_sbs",
-  "2d_hyp"
 ];
 
 export default function AlgorithmsDashboard(props: { currentAlgorithmName: ("gd" | "lr"), currentStep: number, component: any }) {
