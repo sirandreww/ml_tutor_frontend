f7d1f778f50eaac8a81fb5e5db63c99bfb9ee29b
AseelAborokn <AseelAborokn@users.noreply.github.com>
2022-04-08 18:41:50 +0300

    almost at the end - need to return ws/bs/costs of all iterations

--- src/pages/algorithms/logistic_regression/LogReg2.tsx
+++ src/pages/algorithms/logistic_regression/LogReg2.tsx
@@ -2,9 +2,6 @@
 import React from 'react';
 import AlgorithmsDashboard from 'components/AlgorithmsDashboard';
 import LogisticRegression from "components/LogisticRegression/LogisticRegression";
-import { create, all } from 'mathjs';
-
-const math = create(all, {})
 
 export default function LogReg2() {
     return (
@@ -15,14 +12,14 @@ export default function LogReg2() {
                 data_batch={x_train}
                 classifications={y_train}
                 learning_rate={0.0015}
-                total_iterations={1000}
+                total_iterations={5}
             />
             }
         />
     );
 }
 
-const x_train = math.transpose(math.matrix([
+const x_train = [
     [3,0,22.0,1,0,7.25,1],
     [1,1,38.0,1,0,71.2833,0],
     [3,1,26.0,0,0,7.925,1],
@@ -33,6 +30,7 @@ const x_train = math.transpose(math.matrix([
     [3,0,2.0,3,1,21.075,1],
     [3,1,27.0,0,2,11.1333,1],
     [2,1,14.0,1,0,30.0708,0],
-]))
+]
 
-const y_train = math.matrix([0, 1, 1, 1, 0, 0, 0, 0, 1, 1])
+const y_train = [0, 1, 1, 1, 0, 0, 0, 0, 1, 1]
+
