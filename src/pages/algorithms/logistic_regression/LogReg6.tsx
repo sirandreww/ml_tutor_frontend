e510cf473b101ef05ebf3f5fff16c4fbb1fef15b
AseelAborokn <AseelAborokn@users.noreply.github.com>
2022-04-18 19:38:44 +0300

    Code init

--- src/pages/algorithms/logistic_regression/LogReg6.tsx
+++ src/pages/algorithms/logistic_regression/LogReg6.tsx
@@ -1,13 +1,16 @@
 // ------------------------ IMPORTS ------------------------  
 import React from 'react';
 import AlgorithmsDashboard from 'components/AlgorithmsDashboard';
+import LogisticRegressionTraining from "../../../components/logistic_regression/LogisticRegressionTraining";
 
 export default function LogReg6() {
     return (
         <AlgorithmsDashboard
             currentAlgorithmName="logreg"
             currentStep={6}
-            component={ "logreg" }
+            component={
+                <LogisticRegressionTraining />
+            }
         />
     );
 }
