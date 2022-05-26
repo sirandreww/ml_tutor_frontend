6f2a015bb4f02effda578a44660b804ddc93a649
sirandreww <andrew.luka.ia@gmail.com>
2022-05-26 18:20:43 +0300

    footer sticks to bottom

--- src/components/Footer.tsx
+++ src/components/Footer.tsx
@@ -2,28 +2,34 @@ import React from 'react';
 import { Box, Typography } from '@mui/material'
 import { Copyright } from '@mui/icons-material';
 import PrettyLine from 'components/PrettyLine';
-import {useTranslation} from "react-i18next";
+import { useTranslation } from "react-i18next";
 
 function Footer() {
     const [t] = useTranslation('translation');
     return (
-        <>
+        <Box // this is here to allow the footer to stick to the bottom
+            component="footer"
+            sx={{
+                py: 3,
+                px: 2,
+                mt: 'auto',
+                backgroundColor: 'white'
+            }}
+        >
             <PrettyLine />
-            <Box sx={{ bgcolor: 'white', p: 6 }} component="footer">
-                <Typography variant="h6" align="center" gutterBottom color="black">
-                    MLomda
-                </Typography>
-                <Typography
-                    variant="subtitle1"
-                    align="center"
-                    color="black"
-                    component="p"
-                >
-                    {t("footer.made_by")}
-                </Typography>
-                <Copyright />
-            </Box>
-        </>
+            <Typography variant="h6" align="center" gutterBottom color="black">
+                MLomda
+            </Typography>
+            <Typography
+                variant="subtitle1"
+                align="center"
+                color="black"
+                component="p"
+            >
+                {t("footer.made_by")}
+            </Typography>
+            <Copyright />
+        </Box>
     );
 }
 
