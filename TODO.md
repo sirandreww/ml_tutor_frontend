c32a531173f34ec7299c08780eb2f97ddc327ff9
AseelAborokn <AseelAborokn@users.noreply.github.com>
2022-06-04 13:02:23 +0300

    wip

--- TODO.md
+++
@@ -1,7 +0,0 @@
-# Add Linear Regression algorithm - in progress
-
-# Translate to Hebrew - in progress
-
-# Translate to Arabic - For the next sprint?
-
-# Add basic tests - For the next sprint
---
+++ src/components/gradient_descent/__test__/GradientDescentIntroduction2D.test.js
@@ -0,0 +1,29 @@
+import { render, screen} from '@testing-library/react';
+import GradientDescentIntroduction2D from '../GradientDescentIntroduction2D';
+
+describe('Gradient Descent 2D Introduction Rendering', () => {
+
+    it("Should have header with 'Gradient descent 2D' as value", () => {
+        // render(<GradientDescentIntroduction2D/>);
+        // let header = screen.getByTestId("header");
+        // expect(header).toBeTruthy();
+    })
+
+    it("Should render 'how to' section", () => {
+        // render(<GradientDescentIntroduction2D/>);
+        // let header = screen.getByTestId("howto");
+        // expect(header).toBeTruthy();
+    })
+
+    it("Should render 'definition' section", () => {
+        // render(<GradientDescentIntroduction2D/>);
+        // let header = screen.getByTestId("definitions");
+        // expect(header).toBeTruthy();
+    })
+
+    it("Should render 'step' calculations section", () => {
+        // render(<GradientDescentIntroduction2D/>);
+        // let header = screen.getByTestId("steps");
+        // expect(header).toBeTruthy();
+    })
+})
